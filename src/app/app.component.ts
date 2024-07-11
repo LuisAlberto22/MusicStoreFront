61ea2d11fff7e5fcd4c4fd721b3543a37e4852a1
Luis Alberto Garcia Orozco <guichos-mx@outlook.com>
2024-07-10 19:11:05 -0600

    Add Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root', //Esto nos indica como va a ser llamado
   standalone: true, //No se nesecitan los modulos antiguos de angular
-  imports: [RouterOutlet], //Es obligatorio cuando es standalone
+  imports: [HomeComponent], //Es obligatorio cuando es standalone
   //template: '<h1>Hola</h1>', 
   templateUrl: './app.component.html', // este es la ruta del html
   styleUrl: './app.component.css' // ruta de los estilos del html
