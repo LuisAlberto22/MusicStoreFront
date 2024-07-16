039bc0bac1ef6859be8bb256a948ff5602235f20
Luis Alberto Garcia Orozco <guichos-mx@outlook.com>
2024-07-16 10:57:33 -0600

    Create routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root', //Esto nos indica como va a ser llamado
   standalone: true, //No se nesecitan los modulos antiguos de angular
-  imports: [HomeComponent], //Es obligatorio cuando es standalone
+  imports: [HomeComponent, LoginComponent, RouterOutlet], //Es obligatorio cuando es standalone
   //template: '<h1>Hola</h1>', 
   templateUrl: './app.component.html', // este es la ruta del html
   styleUrl: './app.component.css' // ruta de los estilos del html
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../shared/components/footer/footer.component";
+import { SimpleHeaderComponent } from '../shared/components/simple-header/simple-header.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, SimpleHeaderComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
