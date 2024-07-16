1400faa4addc55b0f06609e0706a3b583bb58b77
Luis Alberto Garcia Orozco <guichos-mx@outlook.com>
2024-07-16 10:30:37 -0600

    Change select material and create card

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component } from "@angular/core";
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "../shared/components/footer/footer.component";
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { CardComponent } from '../shared/components/card/card.component';
 
 @Component({
     selector: 'home',
     standalone: true,
-    imports : [HeaderComponent,FooterComponent],
-    templateUrl : './home.component.html',
-    styleUrl : './home.component.css',
+    imports: [HeaderComponent, MatInputModule, FooterComponent, FormsModule, MatSelectModule, MatFormFieldModule, CardComponent],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
 })
 
 export class HomeComponent {
-
 }
